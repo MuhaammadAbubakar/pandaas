c9fe80295830609e80aae690a33cebfe8b4edb5c
MuhammadAbubakar <patanai2412.com>
2026-03-31 23:00:53 +0500

    muhammad abubakar

--- PandasPractice.py
+++ PandasPractice.py
@@ -1,4 +1,20 @@
 import pandas as pd
 
+#load the data and how to read
 df = pd.read_csv(r"C:\Users\rajas\Desktop\pandas practice\pandaas\PIA_2026_Advanced_Kaggle_Dataset.csv")
-print(df)
+
+#print atleast first 5 rows
+print("Print first 5 rows")
+print(df.head(5))
+
+#print atleast last 5 rows
+print("Print last 5 rows")
+print(df.tail(5))
+
+#use info() method to check how mny rows and hw many columns and 
+# data type of each column and also tell how many null values and 
+# also how many memory consumed.
+print(df.info())
+
+#use describe function to get the statistical summary of the data
+print(df.describe())
---
+++ info.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+data = {
+    "name":["raja","rani","ahmed","zia","hira","liaba"],
+    "age":[10,20,30,40,23,21],
+    "salary":[50000,60000,40000,45000,52000,70000],
+    "performance":[72,75,68,89,92,78]
+
+}
+df = pd.DataFrame(data)
+print(df.info())
