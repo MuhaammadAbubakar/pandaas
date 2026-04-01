4d1c02bd422c5408171ba6696a99fe28123bc2f8
MuhammadAbubakar <patanai2412@gmail.com>
2026-04-01 20:10:49 +0500

    update it

--- Shape&Coulmns.py
+++ Shape&Coulmns.py
@@ -1,7 +1,8 @@
+#by using shape and columns we get 
 import pandas as pd
 
 data = pd.read_csv(r"C:\Users\rajas\Desktop\pandas practice\pandaas\PIA_2026_Advanced_Kaggle_Dataset.csv")
 
 print(data)
-#print(data.shape)
+print(data.shape)
 print(data.columns)
