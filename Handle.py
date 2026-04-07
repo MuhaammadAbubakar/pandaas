447efa77a9c07ac312e98f0fb91029c31aa8b021
MuhammadAbubakar <patanai2412@gmail.com>
2026-04-07 23:25:50 +0500

    update it

--- Handle.py
+++ Handle.py
@@ -8,6 +8,7 @@ data = {
     "performance":[72,75,None,89,92,78]
 }
 df = pd.DataFrame(data)
+print(df)
 
 #drop values if we remove specific rows and columns by using axis =0,1
 df.dropna(inplace=True)
